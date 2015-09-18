7ed60598754d4595f2268bb9052faa3b93ec597e
dedong.xu <dedong.xu@goland.cn>
2015-09-18 18:20:54 +0800

    推送通知

---
+++ PHP/demo/guitarwars/newsfeed.php
@@ -0,0 +1,42 @@
+﻿<?php
+	header("COntent-type:text/xml");
+?>
+<?php
+	echo '<?xml version = "1.0" encoding = "utf-8"?>';
+?>
+<rss version = "2.0">
+	<channel>
+		<title>Aliens Abducted Me - Newsfeed</title>
+		<link>http://www.python.org</link>
+		<description>Alien abduction repoprts from Me</description>
+		<language>en-us</language>
+		
+		<item>
+			<title>百度</title>
+			<link>http://www.baidu.com</link>
+			<pubDate>2007</pubDate>
+			<description>11111111111111111111111111111</description>
+		</item>
+		
+		<item>
+			<title>新浪</title>
+			<link>http://www.sina.com</link>
+			<pubDate>2009</pubDate>
+			<description>222222222222222222222222222222</description>
+		</item>
+		
+		<item>
+			<title>腾讯</title>
+			<link>http://www.tencent.com</link>
+			<pubDate>2010</pubDate>
+			<description>33333333333333333333333333333333</description>
+		</item>
+		
+		<item>
+			<title>阿里巴巴</title>
+			<link>http://www.taobao.com</link>
+			<pubDate>2011</pubDate>
+			<description>44444444444444444444444444444444</description>
+		</item>
+	</channel>
+</rss>
