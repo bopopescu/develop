2d4672e25767cd4f49cc58c16f2e8fa188a198aa
buildbot <buildbot@goland.cn>
2017-08-03 12:33:23 +0800

    ajax以POST或者GET方式传送数据

---
+++ Ajax/ajax_post_get_data.js
@@ -0,0 +1,20 @@
+﻿//ajax分别以POST和GET方式来传送数据
+
+$.ajax({
+	type: "POST",
+	url: "/revert/" + id + "/",
+	data: {"changelog": changelog},
+	success: function(data){alert("success")},
+	error: function(data){alert(data)},
+	dataType: "html"
+});
+
+
+$.ajax({
+	type: "GET",
+	url: "/revert/" + id + "/",
+	data: {"changelog": changelog},
+	success: function(data){alert("success")},
+	error: function(data){alert(data)},
+	dataType: "html"
+});
