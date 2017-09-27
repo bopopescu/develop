98c54613c5935c38f908b5ac495e6feddf61e555
unknown <dedong.xu@xudedong.goland.cn>
2017-09-27 09:29:06 +0800

    code publish

--- python/work/Django/code_publish/publish/local_settings.py
+++ python/work/Django/code_publish/publish/local_settings.py
@@ -1,10 +1,10 @@
 #-*- encoding:utf-8 -*-
 
 """ jenkens用户名 """
-username = "jenkins"
+username = "username"
 
 """ jenkens密码 """
-password = "123.com"
+password = "passwd"
 
 """ 可以看到内部失败的人员列表 """
 inner_failed_user_list = ["徐德东", "满潇"]
@@ -13,5 +13,5 @@ inner_failed_user_list = ["徐德东", "满潇"]
 """ 数据库信息 """
 DB_HOST = "localhost"
 DB_USER = "root"
-DB_PASSWD = "sjgldb"
-DB_NAME = "code_publish"
+DB_PASSWD = "passwd"
+DB_NAME = "db"
